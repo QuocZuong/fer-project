1e9c28cbf3809e2e1723ece725244d93924d0cb6
“Zuong” <quocvuongle.ct@gmail.com>
2023-11-02 23:06:44 +0700

    update post feedback and fetch leader

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,14 @@ import { connect } from "react-redux";
 import { actions } from "react-redux-form";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-import { postComment, fetchDishes, fetchComments, fetchPromos } from "../redux/ActionCreators";
+import {
+    postComment,
+    fetchDishes,
+    fetchComments,
+    fetchPromos,
+    fetchLeaders,
+    postFeedback,
+} from "../redux/ActionCreators";
 import Menu from "./MenuComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
@@ -37,6 +44,8 @@ const mapDispatchToProps = (dispatch) => ({
     },
     fetchComments: () => dispatch(fetchComments()),
     fetchPromos: () => dispatch(fetchPromos()),
+    fetchLeaders: () => dispatch(fetchLeaders()),
+    postFeedback: (values) => dispatch(postFeedback(values)),
 });
 
 class Main extends Component {
@@ -55,6 +64,7 @@ class Main extends Component {
         this.props.fetchDishes();
         this.props.fetchComments();
         this.props.fetchPromos();
+        this.props.fetchLeaders();
     }
 
     onDishSelect(dishId) {
@@ -86,7 +96,9 @@ class Main extends Component {
                     promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
                     promoLoading={this.props.promotions.isLoading}
                     promoErrMess={this.props.promotions.errMess}
-                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+                    leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+                    leaderLoading={this.props.leaders.isLoading}
+                    leaderErrMess={this.props.leaders.errMess}
                 />
             );
         };
